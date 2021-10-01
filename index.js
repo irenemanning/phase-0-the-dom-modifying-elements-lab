a29722eab9ebb6b0bdf45a3ebe2eceda09d832a4
Irene <irenemanningm@gmail.com>
2021-10-01 18:19:06 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// Write your code here!
+document.getElementById('main');
+main.remove('main');
+
+const newHeader = document.createElement('h1');
+
+newHeader.setAttribute("id", "victory");
+newHeader.textContent = "Irene is the champion";
+console.log(newHeader.textContent);
+
+document.body.append(newHeader);
